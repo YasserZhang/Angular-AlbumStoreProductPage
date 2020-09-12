4fc65a1307b0d7ba7df87bdd5393068d44f0d328
yaseru2003@gmail.com <yaseru2003@gmail.com>
2020-09-11 23:34:27 -0400

    section 3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,12 +4,11 @@ import 'rxjs/add/operator/map'
 
 @Injectable()
 export class ProductService {
-  private _http: Http;
-  _albumUrl = '../assets/album.json';
-  constructor() { }
+  private _albumUrl = '../assets/album.json';
+  constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-    return this._http.get('../assets/album.json').map(response => response.json())
+    return this._http.get(this._albumUrl).map(response => response.json())
   }
 
 }
